5c7f5f063c18f7ed0aa8e201e6f1bc3e396b60d0
Garrett Price <garrettleeprice@gmail.com>
2016-12-20 15:39:40 -0800

    add slidedown section and modal windows for paper and photo galleries

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,28 @@ $(document).ready(function(){
   $('.js-toggle-rust').click( function() {
     $('.slidedown-rust').toggleClass('active');
     $('.slidedown-etch').removeClass('active');
+    $('.slidedown-paper').removeClass('active');
+    $('.slidedown-photo').removeClass('active');
 
   });
   $('.js-toggle-etch').click( function() {
     $('.slidedown-etch').toggleClass('active');
     $('.slidedown-rust').removeClass('active');
+    $('.slidedown-paper').removeClass('active');
+    $('.slidedown-photo').removeClass('active');
   });
-
+  $('.js-toggle-paper').click( function() {
+    $('.slidedown-paper').toggleClass('active');
+    $('.slidedown-rust').removeClass('active');
+    $('.slidedown-etch').removeClass('active');
+    $('.slidedown-photo').removeClass('active');
+  });
+  $('.js-toggle-photo').click( function() {
+    $('.slidedown-photo').toggleClass('active');
+    $('.slidedown-rust').removeClass('active');
+    $('.slidedown-etch').removeClass('active');
+    $('.slidedown-paper').removeClass('active');
+  });
 });
 
 $(function() {
