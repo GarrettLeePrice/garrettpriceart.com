2a0190201be9b211fc90bc01d692f6d532dccf31
Garrett Price <garrettleeprice@gmail.com>
2016-12-21 16:24:02 -0800

    add images to etch slidedown gallery, connect all modal windows for rust&etch slidedown galleries.

--- js/scripts.js
+++ js/scripts.js
@@ -51,22 +51,3 @@ $(function() {
     });
   });
 });
-
-$(window).resize(function() {
-  var more = document.getElementById("js-navigation-more");
-  if ($(more).length > 0) {
-    var windowWidth = $(window).width();
-    var moreLeftSideToPageLeftSide = $(more).offset().left;
-    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
-
-    if (moreLeftSideToPageRightSide < 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
-    }
-
-    if (moreLeftSideToPageRightSide > 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
-    }
-  }
-});
